d65358f6c31ef0482409d9e4586010213f4b1502
John Hinz <jhinz402@hotmail.com>
2020-02-29 00:02:06 -0500

    Initial Commit

---
+++ Encryptor/StringExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Encryptor
+{
+    public static class StringExtensions
+    {
+
+        public static string EncryptAES(this string clearText, string key)
+        {
+            string EncryptionKey = key;
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+
+        public static string DecryptAES(this string cipherText, string key)
+        {
+            string EncryptionKey = key;
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+    }
+
+
+  
+
+}
---
+++ TastHarness/Program.cs
@@ -0,0 +1,20 @@
+﻿using Encryptor;
+using System;
+
+namespace TastHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello World";
+            string key = "MUMBOJumbo";
+            string encryptedInput = input.EncryptAES(key);
+            string decryptedInput = encryptedInput.DecryptAES(key);
+
+            Console.WriteLine($"Original string: {input}");
+            Console.WriteLine($"Encrypted string: {encryptedInput}");
+            Console.WriteLine($"Decrypted string: {decryptedInput}");
+        }
+    }
+}
